06ef05a9fa8c922574ebd393270a31b1519fe96a
Priyanka Joseph <prjoseph.cva@gmail.com>
2020-07-29 07:35:31 -0700

    Initial Commit

---
+++ Mask Up/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mask Up
+//
+//  Created by Apple on 7/29/20.
+//  Copyright © 2020 Priyanka Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
